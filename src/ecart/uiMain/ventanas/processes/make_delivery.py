94769d8656ff99a8a9345c7558701d57c73eaa83
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-11-20 12:35:02 -0500

    fix(make_delivery)

--- src/ecart/uiMain/ventanas/processes/make_delivery.py
+++ src/ecart/uiMain/ventanas/processes/make_delivery.py
@@ -120,25 +120,17 @@ class MakeDelivery(Base):
                             ("lightpink", "lightyellow", "lightgreen",
                              "lightblue", "lightsalmon")))
 
-        def set_current_delivery(self, use_current: bool = False):
-            if use_current:
-                if not self.is_current_product(): return
-
-            self.destroy()
+        def set_current_delivery(use_current: bool = False):
+            self.current_delivery = delivery
+            self.personnel_pseudoframe.destroy()
             self.header_frame.destroy()
 
-            FORM = ["Nombre", "Precio", "Cantidad", "Descripcion\n"]
+            FORM = ["Escoje el pedido"]
 
             if use_current:
                 if self.current_product:
-                    FORM = [["Nombre", self.current_product.get_name()],
-                            ["Precio", str(self.current_product.get_price())],
-                            ["Cantidad",
-                             str(self.current_product.get_quantity())],
-                            [
-                                "Descripcion\n",
-                                str(self.current_product.get_description())
-                            ]]
+                    orders_list = [str(order) for order in Admin.current.get_current_store().get_orders()]
+                    FORM = [["Pedido", order] for order in orders_list]
 
             form = FieldFrame(master=self,
                               title="Producto",
@@ -152,7 +144,7 @@ class MakeDelivery(Base):
         # put it inside the pseudo frame
         self.personnel_pseudoframe.window_create("end", window=item)
 
-        item.bind("<Button-1>", lambda _: self.set_current_delivery(use_current=False))
+        item.bind("<Button-1>", lambda _: set_current_delivery(use_current=False))
 
     def setup_ui(self):
         self.header_frame = tk.Frame(self, bg="lightblue")
@@ -231,6 +223,87 @@ class MakeDelivery(Base):
         if use_current:
             if not self.is_current_delivery(): return
 
+        self.personnel_pseudoframe.destroy()
+        self.header_frame.destroy()
+
+        FORM = ["Nombre", "Calle", "Carrera"]
+
+        if use_current:
+            if self.current_delivery:
+                FORM = [["Nombre", self.current_delivery.get_name()],
+                        ["Calle",
+                         str(self.current_delivery.get_address()[0])],
+                        ["Carrera",
+                         str(self.current_delivery.get_address()[1])]]
+
+        form = FieldFrame(master=self,
+                          title="Delery",
+                          fields_title="Propiedades",
+                          entries_title="Valores",
+                          fields=FORM,
+                          save_callback=lambda input: errors.pcall(
+                              self.save_callback, form, use_current, input))
+        form.pack(expand=True, fill="both")
+
+    def add_to_grid(self, delivery: Delivery) -> None:
+        item = tk.Label(bd=5,
+                        padx=5,
+                        image=self._icon,
+                        text=delivery.get_name(),
+                        compound=tk.TOP,
+                        font=Commons.HEADER_FONT,
+                        relief="solid",
+                        bg=random.choice(
+                            ("lightpink", "lightyellow", "lightgreen",
+                             "lightblue", "lightsalmon")))
+
+    def setup_ui(self):
+        self.header_frame = tk.Frame(self, bg="lightblue")
+        self.header_frame.pack(fill="x", padx=10, side="top", anchor="center")
+
+        self.personnel_pseudoframe = ScrollableText(self,
+                                                    wrap="char",
+                                                    borderwidth=0,
+                                                    highlightthickness=0,
+                                                    state="disabled",
+                                                    cursor="arrow")
+
+        admin: Admin = Admin.current
+
+        deliveries = admin.get_current_store_deliveries()
+        if deliveries is not None:
+            for delivery in deliveries:
+                self.add_to_grid(delivery)
+
+        self.selected_delivery = tk.Label(self.header_frame,
+                                          text="Delivery seleccionado: ninguno",
+                                          font=Commons.TEXT_FONT_BI,
+                                          bg="lightblue")
+        self.header_frame.columnconfigure(1, weight=1)
+        self.personnel_pseudoframe.pack(fill="both", expand=True, padx=10)
+
+
+    def setup_ui(self):
+        self.instances_pseudoframe = ScrollableText(
+            self,
+            wrap="char",
+            borderwidth=0,
+            highlightthickness=0,
+            state="disabled",
+            cursor="arrow"
+        )
+
+        for delivery in Delivery.get_all_instances():
+            self.add_delivery_to_grid(delivery)
+
+        self.add_button = tk.Button(
+            self,
+            text="   Realizar Entrega   ",
+            command=self.show_field_frame
+        )
+
+        self.add_button.pack(pady=(0, 10))
+        self.instances_pseudoframe.pack(fill="both", expand=True, padx=10)
         self.personnel_pseudoframe.destroy()
         self.header_frame.destroy()
 
