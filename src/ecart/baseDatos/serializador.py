2acb60bb87eaa6d288a42a41cde1be2c3954ef2a
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-11-21 01:07:20 -0500

    fix(serealizador)

--- src/ecart/baseDatos/serializador.py
+++ src/ecart/baseDatos/serializador.py
@@ -1,7 +1,35 @@
 import pickle
 from ecart.gestorAplicacion.merchandise.store import Store
+import os
 
-file = "src/store.pkl"
+
+class StoreSerializer:
+    file = "Store"
+
+    @staticmethod
+    def serialize(store):
+        if os.path.exists(StoreSerializer.file):
+            # El archivo ya existe, cargar datos actuales
+            with open(StoreSerializer.file, "rb") as existing_file:
+                existing_data = pickle.load(existing_file)
+            store.update(existing_data)
+
+        # Guardar datos actualizados
+        with open(StoreSerializer.file, "wb") as picklefile:
+            pickle.dump(store, picklefile)
+
+    @staticmethod
+    def deserialize():
+        if os.path.exists(StoreSerializer.file):
+            with open(StoreSerializer.file, "rb") as picklefile:
+                Store.instances = pickle.load(picklefile)
+                print(type(Store.instances))
+        else:
+            print("El archivo no existe.")
+
+
+"""
+ file = "src/store.pkl"
 
 
 class StoreSerializer:
@@ -20,3 +48,4 @@ class StoreSerializer:
         picklefile.close()
 
         print(type(Store.instances))
+"""
